50e3d21e0767717c88b835f11483a3504c800d05
Laurie Nicholas <laurienicholas@gmail.com>
2020-05-26 08:07:35 +0100

    Rename build dir. Slim out git ignore

---
+++ lib/index.d.ts
@@ -0,0 +1,57 @@
+export declare const prismicPagination: (args: {
+    prismicConnectionName: string;
+    postsPerPage: number;
+    prismicConnectionArgs?: {
+        [key: string]: string;
+    } | undefined;
+    nodeFields?: string[] | undefined;
+    component: string;
+    graphql: (query: string) => Promise<IGraphQlResponse>;
+    createPage: (args: {
+        path: string;
+        component: string;
+        context: {
+            [property: string]: any;
+        };
+    }) => void;
+    pathPrefix: string;
+}) => Promise<{
+    allPosts: {
+        node: {
+            [property: string]: any;
+        };
+    }[];
+    allPages: {
+        edges: {
+            node: {
+                [property: string]: any;
+            };
+        }[];
+        pageInfo: {
+            startCursor: string;
+            endCursor: string;
+            hasNextPage: boolean;
+            hasPreviousPage: boolean;
+        };
+    }[];
+}>;
+interface IGraphQlResponse {
+    data: {
+        prismic: {
+            [connectionName: string]: {
+                edges: {
+                    node: {
+                        [property: string]: any;
+                    };
+                }[];
+                pageInfo: {
+                    startCursor: string;
+                    endCursor: string;
+                    hasNextPage: boolean;
+                    hasPreviousPage: boolean;
+                };
+            };
+        };
+    };
+}
+export {};
